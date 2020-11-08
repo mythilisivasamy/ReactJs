a4ffb64ed3c1396619f64a6890d700063cc1c032
mythili <70125026+mythilisivasamy@users.noreply.github.com>
2020-11-08 18:56:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+﻿import React ,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+import './mystyle.css';
+class MainApp extends Component{
+  
+  
+  render(){
+    return(
+	<div>
+        <h1 class="top-header">Welcome To</h1>
+	<h2 class="top-subheader">React Online class</h2>
+	<p><span>Child of this paragraph</span></p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	<ul>
+	<li>Frameworks</li>
+	<ul>
+	<li>Vue</li>
+	<li>Angular</li>
+	<li>Nested Child</li>
+	</ul>
+	<li>React JS</li>
+	<li>HTML</li>
+	<li>CSS</li>
+	</ul>
+	</div>
+	 	
+  );}
+
+}
+
+ ReactDOM.render(<MainApp/>,document.getElementById('root'));
